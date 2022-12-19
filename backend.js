9acd1b55d28bb9db531a77a91c4d12bfd4a99e62
Divya <79148296+dkk3675@users.noreply.github.com>
2022-12-19 18:37:15 +0530

    Update backend.js

--- backend.js
+++ backend.js
@@ -12,7 +12,7 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 const mongo = process.env.MONGO_URI;
 console.log("Hello",mongo);
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(mongo)
   .then(() => {
     app.listen(process.env.TO_PORT,() => {
       console.log(`Connected to DB at Port ${process.env.TO_PORT}`);
