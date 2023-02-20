abd014eaedc3c19d81205713149afa75e2f4e473
Divya <karan.boss1000@gmail.com>
2023-02-20 09:10:06 +0530

    initial

--- backend.js
+++ backend.js
@@ -13,7 +13,7 @@ mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(process.env.TO_PORT,() => {
-      console.log(`Connected to DB at Port ${process.env.TO_PORT}`);
+      console.log(`Connected to DB at Port ${process.env.PORT}`);
     });
   })
   .catch((err) => {
